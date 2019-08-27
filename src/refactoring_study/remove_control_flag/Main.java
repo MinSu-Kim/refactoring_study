b518aa12da53e8882ae09e363a4816bb4f6fba35
영남인재교사 <net94.teacher@gmail.com>
2019-08-27 15:54:27 +0900

    Properties 사용

--- src/refactoring_study/remove_control_flag/Main.java
+++ src/refactoring_study/remove_control_flag/Main.java
@@ -3,25 +3,46 @@ package refactoring_study.remove_control_flag;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
+	public static void main(String[] args) {
+		try {
 			String fileUrl = Thread.currentThread().getContextClassLoader().getResource("dbfile.txt").getPath();
 
 			SimpleDatabase db = new SimpleDatabase(new FileReader(fileUrl));
 //        	SimpleDatabase db = new SimpleDatabase(new FileReader("dbfile.txt"));
-            Iterator<String> iterator = db.iterator();
-            while(iterator.hasNext()) {
-                String key = iterator.next();
-                System.out.printf("KEY : %s%nVALUE : %s%n%n", key, db.getValue(key));
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+			Iterator<String> iterator = db.iterator();
+			while (iterator.hasNext()) {
+				String key = iterator.next();
+				System.out.printf("KEY : %s%nVALUE : %s%n%n", key, db.getValue(key));
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		loadProperties();
+	}
+
+	private static void loadProperties() {
+		//SimpleDatabase--> Properties·Î º¯°æ
+		String fileUrl = Thread.currentThread().getContextClassLoader().getResource("dbfile.txt").getPath();
+
+		Properties properties = new Properties();
+		try (FileReader reader = new FileReader(fileUrl)) {
+			properties.load(reader);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		for(Entry<Object, Object> e : properties.entrySet()) {
+			System.out.printf("KEY : %s%nVALUE : %s%n%n", e.getKey(), e.getValue());
+		}
+	}
 
 }
--- src/refactoring_study/remove_control_flag/SimpleDatabase.java
+++ src/refactoring_study/remove_control_flag/SimpleDatabase.java
@@ -12,21 +12,25 @@ import java.util.regex.Pattern;
 public class SimpleDatabase {
 	private Map<String, String> map = new HashMap<>();
 
-    private static Pattern pattern = Pattern.compile("([^=]+)=(.*)");
+	private static Pattern pattern = Pattern.compile("([^=]+)=(.*)");
 
-    public SimpleDatabase(Reader r) throws IOException {
-        BufferedReader reader = new BufferedReader(r);
-        String line;
-        
-        while ((line = reader.readLine()) != null) {
-            Matcher matcher = pattern.matcher(line);
-            if (matcher.matches()) {
-                String key = matcher.group(1);
-                String value = matcher.group(2);
-                map.put(key, value);
-            }
-        }
-    }
+	public SimpleDatabase() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public SimpleDatabase(Reader r) throws IOException {
+		BufferedReader reader = new BufferedReader(r);
+		String line;
+
+		while ((line = reader.readLine()) != null) {
+			Matcher matcher = pattern.matcher(line);
+			if (matcher.matches()) {
+				String key = matcher.group(1);
+				String value = matcher.group(2);
+				map.put(key, value);
+			}
+		}
+	}
 
 	public void putValue(String key, String value) {
 		map.put(key, value);
