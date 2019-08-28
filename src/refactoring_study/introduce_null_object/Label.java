341a64365f50d0275629bdb757954b89e64490b6
영남인재교사 <net94.teacher@gmail.com>
2019-08-28 15:11:53 +0900

    널 객체 도입 리팩토링 전

---
+++ src/refactoring_study/introduce_null_object/Label.java
@@ -0,0 +1,22 @@
+package refactoring_study.introduce_null_object;
+
+public class Label {
+	private final String label;
+
+    public Label(String label) {
+        this.label = label;
+    }
+
+    public void display() {
+        System.out.printf("display : %s%n", label);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("\"%s\"", label);
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
