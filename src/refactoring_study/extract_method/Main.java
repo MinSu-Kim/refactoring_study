383a71bf8cd8ca3a0613b3e421b7fbd86f9d6507
영남인재교사 <net94.teacher@gmail.com>
2019-08-29 15:16:30 +0900

    extract_method

---
+++ src/refactoring_study/extract_method/Main.java
@@ -0,0 +1,10 @@
+package refactoring_study.extract_method;
+
+public class Main {
+
+	public static void main(String[] args) {
+        Banner hello = new Banner("Hello, World!");
+        hello.print(3);
+	}
+
+}
