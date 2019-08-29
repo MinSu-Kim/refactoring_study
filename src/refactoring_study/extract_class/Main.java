24d2f38f4946af49d4947d1d521273458a51ca21
영남인재교사 <net94.teacher@gmail.com>
2019-08-29 15:19:17 +0900

    클래스 추출 적용 전

---
+++ src/refactoring_study/extract_class/Main.java
@@ -0,0 +1,17 @@
+package refactoring_study.extract_class;
+
+public class Main {
+
+	public static void main(String[] args) {
+        Book refactoring = new Book(
+                "Refactoring: improving the desgin of existing code", 
+                "ISBN0201485672", 
+                "$4495",
+                "Martin Fowler", 
+                "fowler@acm.org");
+        
+        System.out.println(refactoring.toXml());
+
+	}
+
+}
