572a515457879deec6b38eb3b6af9e5442fbc005
영남인재교사 <net94.teacher@gmail.com>
2019-08-29 15:17:07 +0900

    메서드 추출 적용

--- src/refactoring_study/extract_method/Banner.java
+++ src/refactoring_study/extract_method/Banner.java
@@ -1,30 +1,29 @@
 package refactoring_study.extract_method;
 
 public class Banner {
-	private final String content;
+    private final String content;
 
-	public Banner(String content) {
-		this.content = content;
-	}
+    public Banner(String content) {
+        this.content = content;
+    }
+    
+    public void print(int times) {
+        printBorder();
+        printContent(times);
+        printBorder();
+    }
 
-	public void print(int times) {
-        //테두리 출력
-        System.out.print("+");
-        for(int i=0; i< content.length(); i++) {
-            System.out.print("-");
-        }
-        System.out.println("+");
-        
-        //내용 출력
+    private void printContent(int times) {
         for(int i=0; i<times; i++) {
             System.out.println("|" + content + "|");
         }
-        
-        //테두리출력
+    }
+
+    private void printBorder() {
         System.out.print("+");
         for(int i=0; i< content.length(); i++) {
             System.out.print("-");
         }
         System.out.println("+");
-	}
-}
+    }
+}        
