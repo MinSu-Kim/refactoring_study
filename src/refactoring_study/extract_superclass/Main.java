b268360842c3d11fec7c04dbca96310e7d8c51d0
영남인재교사 <net94.teacher@gmail.com>
2019-08-29 15:25:38 +0900

    상위 클래스 추출 전

---
+++ src/refactoring_study/extract_superclass/Main.java
@@ -0,0 +1,23 @@
+package refactoring_study.extract_superclass;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Player musicPlayer = new Player();
+        musicPlayer.setCurrentMedia(true);
+        
+        Player videoPlayer = new Player();
+        videoPlayer.setCurrentMedia(false);
+        
+        play(musicPlayer);
+        play(videoPlayer);
+    }
+
+    private static void play(Player player) {
+        player.play();
+        player.loop();
+        player.stop();
+    }
+
+
+}
