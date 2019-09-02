398f996ec88134bafd1dcead330379bd9a9fc2c8
영남인재교사 <net94.teacher@gmail.com>
2019-09-02 15:15:18 +0900

    분류 코드를 하위 클래스로 치환 리팩토링 후

--- src/refactoring_study/replace_type_code_with_subclass/Main.java
+++ src/refactoring_study/replace_type_code_with_subclass/Main.java
@@ -6,9 +6,9 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
-		List<Shape> shapes = Arrays.asList(new Shape(Shape.TYPECODE_LINE, 0, 0, 100, 200),
-				new Shape(Shape.TYPECODE_RECTANGLE, 10, 20, 30, 40),
-				new Shape(Shape.TYPECODE_OVAL, 100, 200, 300, 400));
+		List<Shape> shapes = Arrays.asList(Shape.createShape(Shape.TYPECODE_LINE, 0, 0, 100, 200),
+				Shape.createShape(Shape.TYPECODE_RECTANGLE, 10, 20, 30, 40),
+				Shape.createShape(Shape.TYPECODE_OVAL, 100, 200, 300, 400));
 
 		for (Shape s : shapes) {
 			s.draw();
--- src/refactoring_study/replace_type_code_with_subclass/Shape.java
+++ src/refactoring_study/replace_type_code_with_subclass/Shape.java
@@ -1,72 +1,44 @@
 package refactoring_study.replace_type_code_with_subclass;
 
-public class Shape {
+public abstract class Shape {
 	public static final int TYPECODE_LINE = 0;
 	public static final int TYPECODE_RECTANGLE = 1;
 	public static final int TYPECODE_OVAL = 2;
 
-	private final int typeCode;
 	private final int startX;
 	private final int startY;
 	private final int endX;
 	private final int endY;
 
-	public Shape(int typeCode, int startX, int startY, int endX, int endY) {
-		this.typeCode = typeCode;
+    public static Shape createShape(int typeCode, int startX, int startY, int endX, int endY) {
+        switch (typeCode) {
+        case TYPECODE_LINE:
+            return new ShapeLine(startX, startY, endX, endY);
+        case TYPECODE_RECTANGLE:
+            return new ShapeRectangle(startX, startY, endX, endY);
+        case TYPECODE_OVAL:
+            return new ShapeOval(startX, startY, endX, endY);
+        default:
+            throw new IllegalArgumentException("typeCode = " + typeCode);
+        }
+    }
+
+	public Shape(int startX, int startY, int endX, int endY) {
 		this.startX = startX;
 		this.startY = startY;
 		this.endX = endX;
 		this.endY = endY;
 	}
 
-	public int getTypeCode() {
-		return typeCode;
-	}
+	public abstract int getTypeCode();
 
-	public String getName() {
-		switch (typeCode) {
-		case TYPECODE_LINE:
-			return "LINE";
-		case TYPECODE_RECTANGLE:
-			return "RECTANGLE";
-		case TYPECODE_OVAL:
-			return "OVAL";
-		default:
-			return null;
-		}
-	}
+	public abstract String getName();
 
 	@Override
 	public String toString() {
 		return String.format("[%-10s, (%3s, %3s) - (%3s, %3s)]", getName(), startX, startY, endX, endY);
 	}
 
-	private void drawLine() {
-		System.out.printf("%15s : %s%n", "drawLine", this.toString());
-	}
-
-	private void drawRectangle() {
-		System.out.printf("%15s : %s%n", "drawRectangle", this.toString());
-	}
-
-	private void drawOval() {
-		System.out.printf("%15s : %s%n", "drawOval", this.toString());
-	}
-
-	public void draw() {
-		switch (typeCode) {
-		case TYPECODE_LINE:
-			drawLine();
-			break;
-		case TYPECODE_RECTANGLE:
-			drawRectangle();
-			break;
-		case TYPECODE_OVAL:
-			drawOval();
-			break;
-		default:
-			;
-		}
-	}
+	public abstract void draw();
 
 }
